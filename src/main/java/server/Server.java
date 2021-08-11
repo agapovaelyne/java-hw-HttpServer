d2974b6b73cbfa87220572f5128dbefded376923
agapovaelyne <agapova.lina@gmail.com>
2021-08-11 12:28:53 +0300

    Second partt of HW uploaded

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,14 +1,11 @@
 package server;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileSystemException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,14 +16,44 @@ public class Server {
     protected final int PORT = 9999;
     protected final int THREADS_NUMBER = 64;
     protected final ExecutorService serverThreadPool;
+    protected Map<String, Handler> handlers = new ConcurrentHashMap<>();
     protected final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
+
+    protected final static String KEY_SEPARATOR = ":";
     protected final static String NF_ERROR_TEXT = """
             HTTP/1.1 404 Not Found\r
             Content-Length: 0\r
             Connection: close\r
             \r
             """;
+
+    protected final Handler universalHandler = (request, out) -> {
+        try {
+            if (!validPaths.contains(request.getPATH())) {
+                out.write((NF_ERROR_TEXT).getBytes());
+                out.flush();
+            }
+
+            final var filePath = Path.of(".", "public", request.getPATH());
+            final var mimeType = Files.probeContentType(filePath);
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (FileSystemException e) {
+            out.write((NF_ERROR_TEXT).getBytes());
+            out.flush();
+        }
+
+    };
 
     public Server() {
         serverThreadPool = Executors.newFixedThreadPool(THREADS_NUMBER);
@@ -47,59 +74,31 @@ public class Server {
     private void handleConnection(Socket clientSocket) {
         try (
                 clientSocket;
-                final var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                final var out = new BufferedOutputStream(clientSocket.getOutputStream())
+                var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                var out = new BufferedOutputStream(clientSocket.getOutputStream())
         ) {
-            final var requestLine = in.readLine();
-            final var parts = requestLine.split(" ");
 
-            if (parts.length != 3) {
-                out.write((NF_ERROR_TEXT).getBytes());
-                out.flush();
+            var request = Request.parseInputStream(clientSocket.getInputStream());
+            final var path = request.getPATH();
+
+            var handler = handlers.get(request.getMETHOD() + KEY_SEPARATOR + path);
+            if (handler == null) {
+                universalHandler.handle(request, out);
                 return;
             }
 
-            final var path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write((NF_ERROR_TEXT).getBytes());
-                out.flush();
-                return;
-            }
-
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
-
-            // special case for classic
-            if (path.equals("/classic.html")) {
-                final var template = Files.readString(filePath);
-                final var content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
-                out.flush();
-                return;
-            }
-
-            final var length = Files.size(filePath);
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, out);
-            out.flush();
+            handler.handle(request, out);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String requestMethod, String requestedPath, Handler handler) throws IOException {
+        String key = requestMethod + KEY_SEPARATOR + requestedPath;
+        if (handlers.get(key) == null) {
+            handlers.put(key, handler);
+        } else {
+            throw new IOException(String.format("Handler for '%s %s' request already exists!", requestMethod, requestedPath));
         }
     }
 }
